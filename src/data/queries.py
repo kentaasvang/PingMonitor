ccee1de9d9831a1f0361c7415948eea23b82294a
Kent Martin Åsvang <github.com@asvang.no>
2024-10-30 11:46:54 +0100

    updated tables and added insert-query

--- src/data/queries.py
+++ src/data/queries.py
@@ -1,5 +1,6 @@
+from uuid import uuid4
 from .db import _get_cursor
-from .tables import Website
+from .tables import Website, UptimeLogResult
 
 
 def select_websites():
@@ -10,6 +11,28 @@ def select_websites():
 	cur.close()
 
 	return [Website(*website) for website in websites]
+
+
+def insert_uptime_log_result(uptime_log_result: UptimeLogResult):
+    cur = _get_cursor()
+
+    query = f"""
+    INSERT INTO {UptimeLogResult.tablename} (uptime_log_result_id, website_id, date, status_code, response_time)
+    VALUES (?, ?, ?, ?, ?);
+
+    """
+
+    values = (
+            str(uuid4()), 
+            uptime_log_result.website_id, 
+            uptime_log_result.date, 
+            uptime_log_result.status_code,
+            uptime_log_result.response_time
+    )
+
+    cur.execute(query, values)
+    cur.commit()
+    cur.close()
 
 
 if __name__ == "__main__":
--- src/data/tables.py
+++ src/data/tables.py
@@ -1,36 +1,23 @@
-
+# TODO: should I split tables in read and write-tables ?
 
 class Website:
 
-	tablename = "website"
+    tablename = "website"
 
-	def __init__(self, website_id, name, domain):
-		self.website_id = website_id
-		self.name = name
-		self.domain = domain
+    def __init__(self, website_id, name, description, domain):
+        self.website_id = website_id
+        self.name = name
+        self.description = description
+        self.domain = domain
 
 
-class PingResult:
-	def __init__(
-		self,
-		ping_result_id,
-		website_id,
-		date,
-		p_bytes,
-		p_from,
-		ip,
-		icmp_seq,
-		ttl,
-		time
-	):
-		
-		self.ping_result_id = ping_result_id
-		self.website_id = website_id
-		self.date = date
-		self.p_bytes = p_bytes
-		self.p_from = p_from
-		self.ip = ip
-		self.icmp_seq = icmp_seq
-		self.ttl = ttl
-		self.time = time
-		
+class UptimeLogResult:
+
+    tablename = "uptime_log_result"
+
+    def __init__(self, uptime_log_result_id, website_id, date, status_code, response_time):
+        self.uptime_log_result_id = uptime_log_result_id
+        self.website_id = website_id
+        self.date = date
+        self.status_code = status_code
+        self.response_time = response_time
