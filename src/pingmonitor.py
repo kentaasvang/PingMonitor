0d083aff957679721d876224e74b90f4e64e995c
Kent Martin Åsvang <github.com@asvang.no>
2024-10-30 11:47:24 +0100

    foundation is done

--- src/pingmonitor.py
+++ src/pingmonitor.py
@@ -1,20 +1,41 @@
 #!./venv/bin/python
-import os
-from data.queries import select_websites
+# TODO: add logging
+import time
+import datetime
+import requests
+from uuid import uuid4
+from data.queries import select_websites, insert_uptime_log_result
+from data.tables import UptimeLogResult
 
 def main():
-	# get all websites
-	websites = select_websites()
+    # get all websites
+    websites = select_websites()
 
-	# ping all websites
-	for website in websites:
-		result = os.popen(f"ping -c 1 {website.domain}").read()
+    # request all websites
+    for website in websites:
+        uptime_log_result = get_uptime_log_result(website)
+        insert_uptime_log_result(uptime_log_result)
 
-	# parse ping results
-	# store ping results
+    print("Finished.")
 
-	print("hello, world")
+
+def get_uptime_log_result(website):
+
+    start_time = time.time()
+    result = requests.get(website.domain, allow_redirects=True)
+    response_time = time.time() - start_time
+    uptime_vals = (
+            str(uuid4()), 
+            website.website_id, 
+            datetime.datetime.now().isoformat(),
+            result.status_code,
+            response_time
+            )
+
+    uptime_log_result = UptimeLogResult(*uptime_vals)
+    return uptime_log_result
+
 
 
 if __name__ == "__main__": 
-	main()
+    main()
