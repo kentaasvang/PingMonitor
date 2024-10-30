2068a391c09f27d5dbe026ca5baaf1df752b1339
Kent Martin Åsvang <github.com@asvang.no>
2024-10-30 11:03:20 +0100

    altered schema

--- src/data/0001_schema.sql
+++ src/data/0001_schema.sql
@@ -2,22 +2,21 @@
 CREATE TABLE website (
 	website_id TEXT PRIMARY KEY NOT NULL,
 	name TEXT NOT NULL,
-	domain TEXT NOT NULL
+    description TEXT,
+    domain TEXT NOT NULL
 );
 
-
-CREATE TABLE ping_result (
-	ping_result_id TEXT PRIMARY KEY NOT NULL,
-	website_id TEXT NOT NULL,
-	date TEXT CURRENT_TIMESTAMP NOT NULL,
-	bytes INTEGER,
-	[from] TEXT,
-	ip TEXT,
-	icmp_seq INTEGER,
-	ttl INTEGER,
-	time REAL,
-
-	FOREIGN KEY (website_id) REFERENCES website(website_id)
-);
 	
+CREATE TABLE uptime_log_result (
+    uptime_log_result_id TEXT PRIMARY KEY NOT NULL,
+    website_id TEXT NOT NULL,
+	date TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    status_code INTEGER NOT NULL,
+    response_time REAL NOT NULL,
 
+    FOREIGN KEY (website_id) REFERENCES website(website_id)
+);
+
+
+INSERT INTO website (website_id, name, description, domain)
+VALUES ("256a4a81-6cb7-4f87-9cf4-e9c1b12eafb7", "HSOmsorg", "", "www.hsomsorg.no");
